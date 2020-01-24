07a2194800eb621d297d26a2710924c730b47db6
Bilal Tekin <bilal.tekin@boun.edu.tr>
2020-01-24 14:06:49 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -104,7 +104,7 @@ router.post('/login',(req,res,next)=>{
 // @@@@@@@@@@@@ LOGOUT POST REQUEST  @@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 
-router.get('/logout', authenticate,(req,res)=>{
+router.get('/logout',(req,res)=>{
     req.logOut();
     req.flash("successMessage","You Are Successfully Logged Out");
     res.redirect('/');
